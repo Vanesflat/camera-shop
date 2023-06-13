038e21b50433979d6662e26559a60e1dd0e802d0
Vanesflat <ptptcptptc@yandex.ru>
2023-06-13 22:18:40 +0700

    Refactor modals

---
+++ src/components/modal/modal.tsx
@@ -0,0 +1,53 @@
+import cn from 'classnames';
+import { useCallback, useEffect, useRef } from 'react';
+
+type ModalProps = {
+  isOpen: boolean;
+  onCloseClick: () => void;
+  children: React.ReactNode;
+};
+
+function Modal({ isOpen, onCloseClick, children }: ModalProps): JSX.Element {
+  const modalRef = useRef(null);
+
+  const onEscapeKeydown = useCallback((evt: KeyboardEvent) => {
+    if (evt.key === 'Escape') {
+      onCloseClick();
+    }
+  }, [onCloseClick]);
+
+  useEffect(() => {
+    if (isOpen && modalRef.current) {
+      document.body.style.overflow = 'hidden';
+      document.addEventListener('keydown', onEscapeKeydown);
+    }
+
+    return () => {
+      document.body.style.overflow = '';
+      document.removeEventListener('keydown', onEscapeKeydown);
+    };
+  }, [isOpen, onEscapeKeydown]);
+
+  return (
+    <div className={cn('modal', isOpen && 'is-active')} ref={modalRef}>
+      <div className="modal__wrapper">
+        <div className="modal__overlay" onClick={onCloseClick}></div>
+        <div className="modal__content">
+          {children}
+          <button
+            className="cross-btn"
+            type="button"
+            aria-label="Закрыть попап"
+            onClick={onCloseClick}
+          >
+            <svg width="10" height="10" aria-hidden="true">
+              <use xlinkHref="#icon-close"></use>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
--- src/components/review-modal/review-modal.tsx
+++ src/components/review-modal/review-modal.tsx
@@ -1,5 +1,4 @@
-import cn from 'classnames';
-import { useCallback, useEffect } from 'react';
+import Modal from '../modal/modal';
 import ReviewForm from '../review-form/review-form';
 
 type ReviewModalProps = {
@@ -8,45 +7,11 @@ type ReviewModalProps = {
 };
 
 function ReviewModal({ isOpen, onCloseClick }: ReviewModalProps): JSX.Element {
-  const onEscapeKeydown = useCallback((evt: KeyboardEvent) => {
-    if (evt.key === 'Escape') {
-      onCloseClick();
-    }
-  }, [onCloseClick]);
-
-  useEffect(() => {
-    if (isOpen) {
-      document.body.style.overflow = 'hidden';
-      document.addEventListener('keydown', onEscapeKeydown);
-    } else {
-      document.body.style.overflow = '';
-    }
-
-    return () => {
-      document.removeEventListener('keydown', onEscapeKeydown);
-    };
-  }, [isOpen, onEscapeKeydown]);
-
   return (
-    <div className={cn('modal', isOpen && 'is-active')}>
-      <div className="modal__wrapper">
-        <div className="modal__overlay" onClick={onCloseClick}></div>
-        <div className="modal__content">
-          <p className="title title--h4">Оставить отзыв</p>
-          <ReviewForm isOpen={isOpen} onClose={onCloseClick} />
-          <button
-            className="cross-btn"
-            type="button"
-            aria-label="Закрыть попап"
-            onClick={onCloseClick}
-          >
-            <svg width="10" height="10" aria-hidden="true">
-              <use xlinkHref="#icon-close"></use>
-            </svg>
-          </button>
-        </div>
-      </div>
-    </div>
+    <Modal isOpen={isOpen} onCloseClick={onCloseClick}>
+      <p className="title title--h4">Оставить отзыв</p>
+      <ReviewForm isOpen={isOpen} onClose={onCloseClick} />
+    </Modal>
   );
 }
 
